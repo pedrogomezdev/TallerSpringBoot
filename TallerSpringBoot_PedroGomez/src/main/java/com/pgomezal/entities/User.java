1e93bec330306e5dc4915cc714f950ae6ec0035e
Pedro <pedro1234gomez@hotmail.com>
2023-04-20 00:43:59 +0200

    Añado las etiquetas de entidad a User y realizo los insert iniciales en la tabla usuarios.

--- TallerSpringBoot_PedroGomez/src/main/java/com/pgomezal/entities/User.java
+++ TallerSpringBoot_PedroGomez/src/main/java/com/pgomezal/entities/User.java
@@ -1,5 +1,61 @@
 package com.pgomezal.entities;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "birthdate")
+	private LocalDate birthDate;
+
+	public User() {
+		super();
+	}
+
+	public User(Long id, String name, LocalDate birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
 
 }
