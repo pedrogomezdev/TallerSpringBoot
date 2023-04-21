d745b225c739b068623f1cab8cb9a9c9b75527a1
Pedro <pedro1234gomez@hotmail.com>
2023-04-21 20:20:26 +0200

    Elimino clase innecesaria y simplifico UserControllerAdvice. No he sido capaz de realizar el paso 3 del último ejercicio.

--- TallerSpringBoot_PedroGomez/src/main/java/com/pgomezal/controller/advice/UserControllerAdvice.java
+++ TallerSpringBoot_PedroGomez/src/main/java/com/pgomezal/controller/advice/UserControllerAdvice.java
@@ -8,31 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.pgomezal.exceptions.ErrorResponse;
 import com.pgomezal.exceptions.UserValidationException;
 
 @ControllerAdvice
-public class UserControllerAdvice extends ResponseEntityExceptionHandler{
+public class UserControllerAdvice extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(value = {UserValidationException.class})
-	protected ResponseEntity<Object> handleConflict(UserValidationException ex, WebRequest request){
-		String bodyOfResponse = "Falta el nombre y/o la fecha de nacimiento.";
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	@ExceptionHandler(value = { UserValidationException.class })
+	protected ResponseEntity<Object> handleConflict(UserValidationException exception, WebRequest request) {
+		String errorResponse = "Falta el nombre y/o la fecha de nacimiento.";
+
+		return handleExceptionInternal(exception, errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
-	
-	
-	@ExceptionHandler(value = {Exception.class})
-    protected ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
-        ErrorResponse errorResponse = new ErrorResponse(
-                ex.getClass().getSimpleName(),
-                "Unexpected error",
-                HttpStatus.BAD_REQUEST.value(),
-                "An unexpected error occurred"
-        );
-        return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-    }
-	
-	
-	
-	
+
+	@ExceptionHandler(value = { Exception.class })
+	protected ResponseEntity<Object> handleAllExceptions(Exception exception, WebRequest request) {
+		String errorResponse = "PRUEBA";
+
+		return handleExceptionInternal(exception, errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+
 }
